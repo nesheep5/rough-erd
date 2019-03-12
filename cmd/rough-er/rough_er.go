5aa74864be211e1ee430c2cbe686fec7d50be2b9
Shogo Mizuno <shogo.mizuno@dena.com>
2019-03-12 18:42:33 +0900

    edit cmd

--- cmd/rough-er/rough_er.go
+++ cmd/rough-er/rough_er.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/nesheep5/checo"
 	"github.com/urfave/cli"
 )
 
@@ -18,44 +17,11 @@ func main() {
 
 	app.Commands = []cli.Command{
 		{
-			Name:    "list",
+			Name:    "run",
 			Aliases: []string{"l"},
-			Usage:   "Shows checking SNS list",
+			Usage:   "run",
 			Action: func(c *cli.Context) error {
-				fmt.Println("Checking SNS list:")
-				for _, ch := range checo.CheckerMap {
-					fmt.Printf("  %v", ch.Name)
-				}
-				return nil
-			},
-		},
-		{
-			Name:    "check",
-			Aliases: []string{"c"},
-			Usage:   "Checking SNS account",
-			Action: func(c *cli.Context) error {
-				account := c.Args().Get(0)
-				if account == "" {
-					fmt.Errorf("account is required.")
-				}
-
-				fmt.Printf("Search Account: %v \n\n", account)
-
-				for _, c := range checo.CheckerMap {
-					exists, err := c.Exists(account)
-					if err != nil {
-						return err
-					}
-
-					var msg string
-					if exists {
-						msg = "Oops! Alrady Exists..."
-					} else {
-						msg = "OK! No Exists!"
-					}
-
-					fmt.Printf("%v : %v \n", c.Name, msg)
-				}
+				fmt.Println("run!!!")
 				return nil
 			},
 		},
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/nesheep5/rough-er
 
 go 1.12
 
-require github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/urfave/cli v1.20.0
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
