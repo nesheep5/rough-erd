5466e5cdcefe7741d077d3aaf145b078100be174
Shogo Mizuno <shogo.mizuno@dena.com>
2019-03-13 13:26:34 +0900

    edit command

--- cmd/rough-er/rough_er.go
+++ cmd/rough-er/rough_er.go
@@ -17,9 +17,28 @@ func main() {
 
 	app.Commands = []cli.Command{
 		{
-			Name:    "run",
-			Aliases: []string{"l"},
-			Usage:   "run",
+			Name:    "make",
+			Aliases: []string{"m"},
+			Usage:   "make ER diagram.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "database, d",
+					Value: "MySQL",
+					Usage: "use Database.",
+				},
+				cli.StringFlag{
+					Name: "user, u",
+					Usage: "database user",
+				},
+				cli.StringFlag{
+					Name: "password, p",
+					Usage: "database password",
+				},
+				cli.StringFlag{
+					Name: "port, P",
+					Usage: "database port",
+				},
+			},
 			Action: func(c *cli.Context) error {
 				fmt.Println("run!!!")
 				return nil
